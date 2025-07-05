53bf052bdae4840115a079e4d799840d57affaab
Rio-awsm <mm.saha1982@gmail.com>
2025-07-06 04:04:53 +0530

    feat: add Redis integration and user routes to user microservice

--- server/user/src/index.ts
+++ server/user/src/index.ts
@@ -1,11 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
+import { createClient } from "redis";
 import connectToDB from "./config/db.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 connectToDB();
 
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+// redisClient
+//   .connect()
+//   .then(() => console.log("Connected to Redis"))
+//   .catch(console.error);
+
 const app = express();
+app.use("/api/v1", userRoutes)
 
 const port = process.env.PORT;
 
