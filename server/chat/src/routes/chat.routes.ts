827ffc8b29d031ebb45bb24b14695626e1e48547
Rio-awsm <mm.saha1982@gmail.com>
2025-07-23 02:58:30 +0530

    send message and update latest message in db

--- server/chat/src/routes/chat.routes.ts
+++ server/chat/src/routes/chat.routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { createNewChat, getAllChats } from "../controllers/chat.controllers.js";
+import {
+    createNewChat,
+    getAllChats,
+    sendMessage,
+} from "../controllers/chat.controllers.js";
 import { isAuth } from "../middlewares/isAuth.js";
+import { upload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.post("/chat/new", isAuth, createNewChat)
-router.get("/chat/all", isAuth, getAllChats)
+router.post("/chat/new", isAuth, createNewChat);
+router.get("/chat/all", isAuth, getAllChats);
+router.post("/message", isAuth, upload.single("image"), sendMessage);
 
-export default router
+export default router;
