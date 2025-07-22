aa7df9e0c25f74f161a82708df32d0ea5258cb76
Rio-awsm <mm.saha1982@gmail.com>
2025-07-23 02:19:16 +0530

    get chats between users

--- server/chat/src/controllers/chat.controllers.ts
+++ server/chat/src/controllers/chat.controllers.ts
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { AuthenticatedRequest } from "../middlewares/isAuth.js";
 import { Chat } from "../models/chat.model.js";
+import { Messages } from "../models/messages.model.js";
 import TryCatch from "../utils/TryCatch.js";
 
 export const createNewChat = TryCatch(
@@ -36,3 +38,59 @@ export const createNewChat = TryCatch(
     });
   }
 );
+
+export const getAllChats = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const userId = req.user?._id;
+  if (!userId) {
+    res.status(400).json({
+      message: "UserId Missing",
+    });
+    return;
+  }
+
+  const chats = await Chat.find({ users: userId }).sort({ updatedAt: -1 });
+
+  const chatWithUserData = await Promise.all(
+    chats.map(async (chat) => {
+      const otherUserId = chat.users.find((id) => id !== userId);
+
+      const unseenCount = await Messages.countDocuments({
+        chatId: chat._id,
+        sender: { $ne: userId },
+        seen: false,
+      });
+
+      try {
+        const { data } = await axios.get(
+          `${process.env.USER_SERVICE}/api/v1/user/${otherUserId}`
+        );
+
+        return {
+          user: data,
+          chat: {
+            ...chat.toObject(),
+            latestMessage: chat.latestMessage || null,
+            unseenCount,
+          },
+        };
+      } catch (error) {
+        console.log(error);
+        return {
+          user: {
+            _id: otherUserId,
+            name: "Unknown User",
+          },
+          chat: {
+            ...chat.toObject(),
+            latestMessage: chat.latestMessage || null,
+            unseenCount,
+          },
+        };
+      }
+    })
+  );
+
+  res.json({
+    chats: chatWithUserData,
+  });
+});
